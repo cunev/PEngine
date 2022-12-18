84f60c848a5caa9323f4d4f9a285b7199b0c212b
cunev <cunevnew@gmail.com>
2022-12-19 00:09:06 +0200

    test 2

--- app.ts
+++ app.ts
@@ -14,7 +14,7 @@ export let win: Window;
 export let dt: number = 0;
 
 async function main() {
-  // await connectTestserver();
+  await connectTestserver();
   await loadAssets();
   createWindow();
 }
@@ -22,28 +22,29 @@ async function main() {
 App.fps = 144;
 
 function createWindow() {
-  // win = new Window(1920, 1080, { left: 1920, top: 0 });
-  // win.title = "Pretend Engine";
-  // win.fullscreen = true;
-  // ctx = win.canvas.getContext("2d");
-  // InputManager.createInput();
-  // Inventory.create();
-  // let entity = new PlayerEntity();
-  // Camera.focus(entity);
-  // let lastTime = Date.now();
-  // win.on("draw", () => {
-  //   ctx.fillStyle = "#1E1E1E";
-  //   ctx.fillRect(0, 0, 1920, 1080);
-  //   dt = (Date.now() - lastTime) / 8;
-  //   lastTime = Date.now();
-  //   Camera.update();
-  //   for (const entity of Entity.all) {
-  //     entity.update();
-  //     entity.draw();
-  //   }
-  //   Inventory.update();
-  //   TWEEN.update();
-  // });
+  win = new Window(1920, 1080, { left: 1920, top: 0 });
+  win.title = "Pretend Engine";
+  win.fullscreen = true;
+  ctx = win.canvas.getContext("2d");
+  InputManager.createInput();
+  Inventory.create();
+  let entity = new PlayerEntity();
+  Camera.focus(entity);
+  let lastTime = Date.now();
+  win.on("draw", () => {
+    Client.startListening();
+    ctx.fillStyle = "#1E1E1E";
+    ctx.fillRect(0, 0, 1920, 1080);
+    dt = (Date.now() - lastTime) / 8;
+    lastTime = Date.now();
+    Camera.update();
+    for (const entity of Entity.all) {
+      entity.update();
+      entity.draw();
+    }
+    Inventory.update();
+    TWEEN.update();
+  });
 }
 
 main();
--- src/core/Steam.ts
+++ src/core/Steam.ts
@@ -1,69 +1,90 @@
 const GAMEID = 2250500;
 import { init, SteamCallback } from "steamworks.js";
-import rl from "readline";
 export const client = init(GAMEID);
-client.callback.register(SteamCallback.P2PSessionRequest, ({ remote }) => {
-  console.log(`P2PSessionRequest from ${remote}`);
-  client.networking.acceptP2PSession(remote);
-});
 
-client.callback.register(
-  SteamCallback.P2PSessionConnectFail,
-  ({ remote, error }) => {
-    console.log(`Failed to connect to ${remote} with error ${error}`);
+interface MultiplayerMessage {
+  steamId: string;
+  data: Record<string, any>;
+}
+
+export class Client {
+  static isHost: boolean = false;
+  static isConnected: boolean = false;
+  static lobby: Awaited<ReturnType<typeof client.matchmaking.createLobby>>;
+
+  static startListening() {
+    let packetSize: number;
+    while ((packetSize = client.networking.isP2PPacketAvailable()) > 0) {
+      console.log(packetSize);
+
+      const packet = client.networking.readP2PPacket(packetSize);
+      console.log(packet.data.toString());
+      //   if (this.isHost) this.broadcast(packet.data);
+    }
   }
-);
 
-const rlInterface = rl.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+  static broadcast(message: Buffer) {
+    if (!this.isHost) {
+      return;
+    }
+    console.log("broadcasting packet");
 
-rlInterface.question(
-  "Enter a lobby id or press enter to create one: ",
-  async (lobbyId) => {
-    let lobby;
-    if (lobbyId) {
-      lobby = await client.matchmaking.joinJobby(BigInt(lobbyId));
-      lobby.getMembers().forEach((peer) => {
-        client.networking.sendP2PPacket(
-          peer.steamId64,
-          client.networking.SendType.Reliable,
-          Buffer.from("Connection request")
-        );
-      });
-    } else {
-      lobby = await client.matchmaking.createLobby(
-        client.matchmaking.LobbyType.Public,
-        10
+    const members = this.lobby.getMembers();
+    for (const member of members) {
+      if (member.accountId == this.lobby.getOwner().accountId) continue;
+
+      client.networking.sendP2PPacket(
+        member.steamId64,
+        client.networking.SendType.Reliable,
+        message
       );
-      console.log(`Created lobby with id ${lobby.id}`);
+    }
+  }
+
+  static send(message: string) {
+    if (this.isHost) {
+      this.broadcast(Buffer.from(message));
+      return;
     }
 
-    let askChatMessage;
-    askChatMessage = () => {
-      rlInterface.question(client.localplayer.getName() + ": ", (line) => {
-        const message = client.localplayer.getName() + ": " + line;
-        lobby.getMembers().forEach((peer) => {
-          client.networking.sendP2PPacket(
-            peer.steamId64,
-            client.networking.SendType.Reliable,
-            Buffer.from(message)
-          );
-        });
-        askChatMessage();
-      });
-    };
-    askChatMessage();
-
-    setInterval(() => {
-      let size;
-      while ((size = client.networking.isP2PPacketAvailable()) > 0) {
-        const { steamId, data } = client.networking.readP2PPacket(size);
-        if (steamId.steamId64 !== client.localplayer.getSteamId().steamId64) {
-          console.log(data.toString() + "\n");
-        }
-      }
-    }, 66);
+    console.log("sending packet");
+    const host = this.lobby.getOwner();
+    client.networking.sendP2PPacket(
+      host.steamId64,
+      client.networking.SendType.Reliable,
+      Buffer.from(message)
+    );
   }
-);
+}
+
+export async function connectTestserver() {
+  const lobbies = await client.matchmaking.getLobbies();
+  let lobby: Awaited<ReturnType<typeof client.matchmaking.createLobby>>;
+  if (lobbies.length == 0) {
+    lobby = await client.matchmaking.createLobby(
+      client.matchmaking.LobbyType.Public,
+      8
+    );
+  } else {
+    lobby = lobbies[0];
+  }
+
+  lobby.join();
+
+  if (lobby.getOwner().accountId == client.localplayer.getSteamId().accountId) {
+    Client.isHost = true;
+  }
+
+  Client.isConnected = true;
+  Client.lobby = lobby;
+
+  lobby.getMembers().forEach((peer) => {
+    client.networking.sendP2PPacket(
+      peer.steamId64,
+      client.networking.SendType.Reliable,
+      Buffer.from("HELLO from me" + client.localplayer.getName())
+    );
+  });
+
+  Client.startListening();
+}
